16f9a710e6c316b91a22a08d2f7179b2d33cd844
Tim Jarratt <tjarratt@gmail.com>
2014-01-14 23:51:03 -0800

    Prints out a struct implementing given interface
    Still needs some given Returns and Received embedded structs so that you can
    stub out some method calls and / or  inspect the received args.
    
    Probably would also be nice to expose some methods eg: WasCalled or CallCount
    but that's still very forward thinking.

--- examples/fake.go
+++ examples/fake.go
@@ -3,14 +3,18 @@ package fakery
 type Foobar struct {}
 
 type CoolThingToTest interface {
-  MethodToTest(param string) (result bool) // `sugar:accessor`
+  MethodToTest(param string, param2 int32) (result bool, result2 string)
+	IGuessThisIsOkayToo(takesAParam string)
+	AndThisIsSomethingAsWell() (returningValue bool)
+	EdgeCasesYay(string) (bool)
+	AndMoreEdgeCasesAreFunToo()
 }
 
 type CoolThing struct {
   foo Foobar
 }
 
-func (thing CoolThing) MethodToTest(param string) (result bool) {
+func (thing CoolThing) MethodToTest(param string, param2 int32) (result bool, result2 string) {
   if param == "" {
     result = doer.foo.String()
   } else {
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"os"
 	"fmt"
+	"strings"
 	"go/ast"
 	"go/token"
 	"go/parser"
+	"path/filepath"
 )
 
 func main() {
@@ -64,5 +66,71 @@ func sprinkleSugarOn(pathToFile, interfaceToMock string) {
 		return true
 	})
 
-	fmt.Printf("inside package %s, interface is %#v\n", packageName, interfaceNode)
+	fmt.Printf("\nmocking out %s.%s Interface\n", packageName, interfaceToMock)
+
+	outputFileName := filepath.Base(pathToFile)
+	outputDir := filepath.Dir(pathToFile)
+	outputFilePath := filepath.Join(outputDir, "fake_" + outputFileName)
+
+	fmt.Printf("Writing a mock interface to %s\n\n", outputFilePath)
+
+	fakeMock := DeclareFakeImplementingInterface(interfaceToMock, interfaceNode)
+	fmt.Printf("fake implementation looks like:\n**********\n%s\n**********\n", fakeMock)
+}
+
+func DeclareFakeImplementingInterface(name string, anInterface *ast.InterfaceType) string {
+	fakeMethods := ""
+	for _, method := range anInterface.Methods.List {
+		var params, returns string
+		var funcNode *ast.FuncType
+		funcNode = method.Type.(*ast.FuncType)
+
+		if funcNode.Params != nil && funcNode.Params.List != nil&& len(funcNode.Params.List) > 0 {
+			paramsSlice := []string{}
+			for _, p := range funcNode.Params.List {
+				var paramName, paramPair string
+				paramType := string(p.Type.(*ast.Ident).Name)
+
+				if len(p.Names) > 0 {
+					paramName = fmt.Sprintf("%s", p.Names[0])
+					paramPair = strings.Join([]string{paramName, paramType}, " ")
+				} else {
+					paramPair = paramType
+				}
+
+				paramsSlice = append(paramsSlice, paramPair)
+			}
+
+			params = strings.Join(paramsSlice, ", ")
+		}
+
+		if funcNode.Results != nil && funcNode.Results.List != nil && len(funcNode.Results.List) > 0 {
+			resultsSlice := []string{}
+			for _, r := range funcNode.Results.List {
+				var returnName, returnPair string
+				returnType := string(r.Type.(*ast.Ident).Name)
+
+				if len(r.Names) > 0 {
+					returnName = fmt.Sprintf("%s", r.Names[0])
+					returnPair = strings.Join([]string{returnName, returnType}, " ")
+				} else {
+					returnPair = returnType
+				}
+
+				resultsSlice = append(resultsSlice, returnPair)
+			}
+
+			returns = strings.Join(resultsSlice, ", ")
+		}
+
+		fakeMethods = fmt.Sprintf("%s\t%s (%s) (%s)\n", fakeMethods, method.Names[0].Name, params, returns)
+	}
+
+	return fmt.Sprintf("type Fake%s struct {\n%s}", name, fakeMethods)
+}
+
+func StubbedMethodsForInterface(anInterface *ast.InterfaceType) string {
+  formatString := "func (fake %s) %s (%s) (%s) {\n%s\n}"
+	args := []interface{}{}
+	return fmt.Sprintf(formatString, args...)
 }
